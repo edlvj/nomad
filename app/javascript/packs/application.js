a5a383ab559af499d51f0ea2e7f0c930f2d9841d
Ed Gorach <gorachedik96@gmail.com>
2017-08-13 11:59:19 +0000

    fix url

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,8 +14,10 @@ import Translate from './translation'
 import { ApolloClient, createNetworkInterface } from 'apollo-client'
 import VueApollo from 'vue-apollo'
 
+const root_url = process.env.NODE_ENV == 'development' ? 'http://bezviz-edlvj.c9users.io/' : 'https://nomad-ua.herokuapp.com/'
+
 const networkInterface = createNetworkInterface({
-  uri: 'http://bezviz-edlvj.c9users.io:8080/graphql',
+  uri: root_url + '/graphql',
   transportBatching: true,
 });
 
